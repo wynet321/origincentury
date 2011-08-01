9f6db614db90ab826b9c2ca28e4b2c4e7ed8657b
wynet321@gmail.com <wynet321@gmail.com@88a944d0-903e-fc59-a3b1-5ed36b976325>
2011-08-01 08:13:43 +0000

    1.revise Db() mysqli_connect($this->server,$this->user,$this->password,$dbname); to mysqli_connect($this->Server,$this->User,$this->Password,$dbname);
    2.add js folder

--- trunk/lib/login.php
+++ trunk/lib/login.php
@@ -1,8 +1,11 @@
 <?php
-include 'newdb.php';
+include 'db.inc';
 
 $db=new Db();
-#$a=$db->getResultArray("select * from table1;");
+$a=$db->getResultArray("select * from table1;");
+echo "1.";
+echo $a[0][1];
+echo "2.";
 echo $db->getResultArrayRowsNum("select * from table1 where id='1000';");
 $db->close();
 
